83d05b1719d45ec7134f3a96bfdcaf8c2cdce286
naidud <naidud@purdue.edu>
2014-11-17 21:30:48 -0500

    I commented the code where anything closes, then I went through and wrote where I thought the code had an issue with the socket closing
    public void respondToClient(Socket client, int queIndex) throws
    IOException {
    try {
    // Create Output Streams
    PrintWriter outQue = new
    PrintWriter(requestQue.get(queIndex).client.getOutputStream());
    outQue.flush();
    PrintWriter outReq = new PrintWriter(client.getOutputStream());
    outReq.flush();
    
    outQue.print("SOMETHING OUT");
    outReq.print("SOMETHING OUT");
    
    outQue.close(); // Close Stream (3rd close)
    outReq.close(); // Close Stream (4th close)
    requestQue.get(queIndex).client.close(); // Close Que Socket (5th close)
    
    requestQue.remove(queIndex); // Remove Que Person From Que
    client.close(); // Close Requester Socket (6th close) //***Is this where
    the error is?***

--- SafeWalkServer.java
+++ SafeWalkServer.java
@@ -13,20 +13,20 @@ import java.util.*;
 
 public class SafeWalkServer implements Runnable {
     public static void main(String[] args) throws IOException {
-	    try {
-	        SafeWalkServer server;
-	        // Constructor
-	        if(args.length > 0) {
-	            server = new SafeWalkServer(Integer.parseInt(args[0]));
-	        } else {
-	            server = new SafeWalkServer();
-	        }
-	        
-	        // Handle Connections
-	        new Thread(server).start();
+     try {
+         SafeWalkServer server;
+         // Constructor
+         if(args.length > 0) {
+             server = new SafeWalkServer(Integer.parseInt(args[0]));
+         } else {
+             server = new SafeWalkServer();
+         }
+         
+         // Handle Connections
+         new Thread(server).start();
         } catch (IOException e) {
             e.printStackTrace();
-    	}
+     }
     }
     
     private ServerSocket ss;
@@ -34,7 +34,7 @@ public class SafeWalkServer implements Runnable {
     
     ///// Constructors /////
     public SafeWalkServer(int port) throws IOException {
-	    requestQue = new ArrayList<RequestQueObject>();
+     requestQue = new ArrayList<RequestQueObject>();
         try {
             if (port < 1025 || port > 65535) { // Validate Port Number
                 System.out.println("ERROR: Invalid Port Number");
@@ -48,14 +48,14 @@ public class SafeWalkServer implements Runnable {
         }
     }
     public SafeWalkServer() throws IOException {
-	    requestQue = new ArrayList<RequestQueObject>();
-	    try {
-	        ss = new ServerSocket();
-	        ss.setReuseAddress(true);
-	        System.out.println("Port not specified. Using free port " + getLocalPort() + ".");
-    	} catch (IOException e) {
+     requestQue = new ArrayList<RequestQueObject>();
+     try {
+         ss = new ServerSocket();
+         ss.setReuseAddress(true);
+         System.out.println("Port not specified. Using free port " + getLocalPort() + ".");
+     } catch (IOException e) {
             e.printStackTrace();
-    	}
+     }
     }
     
     public int getLocalPort() {
@@ -63,11 +63,11 @@ public class SafeWalkServer implements Runnable {
     }
     
     public void run() {
-	    try {
-	        while (true) {
-	            Socket client = ss.accept(); // Wait For Socket Connection
-	            handleClient(client); // Handle Client
-	        }
+     try {
+         while (true) {
+             Socket client = ss.accept(); // Wait For Socket Connection
+             handleClient(client); // Handle Client
+         }
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -81,23 +81,24 @@ public class SafeWalkServer implements Runnable {
             // Read Client String
             String request = (String) in.readLine();
             
-            // Close Stream
+            // Close Stream (1st close)
             in.close();
             
             // Check Request Validity
             if(checkRequest(request)) { // Request Valid
-	            if(request.charAt(0) == ':') { // Request starts with :
-		            handleCommand(client,request);
-				} else {
-                	handleRequest(client,request);
-				}
+             if(request.charAt(0) == ':') { // Request starts with :
+              handleCommand(client,request);
+    } else {
+                 handleRequest(client,request);
+    }
             } else {
-	            client.close(); // Invalid Command, Close Client
-	        }
+             client.close(); // Invalid Command, Close Client (2nd close) //makes sense to close here since user handled, and requester handled
+         }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
     
     private boolean checkRequest(String request) { // Validity Check
         if(request.charAt(0) == ':') { // Request starts with :
@@ -125,135 +126,136 @@ public class SafeWalkServer implements Runnable {
     
     
     public void handleRequest(Socket client, String request) throws IOException {
-	    try {
-		    String[] requestList = request.split(",");
-	        // Check if match in que
-	        for (int i = 0; i < requestQue.size(); i++) {
-		        if (requestQue.get(i).from.equals(requestList[1])) { // FROM Locations Match
-			        if (requestQue.get(i).to.equals(requestList[2])) { // To Locations Match
-				        respondToClient(client, i);
-				    }
-				    if (requestQue.get(i).to.equals("*") && !requestList[2].equals("*")) { // Que Is Helper
-					    respondToClient(client, i);
-					}
-				    if (!requestQue.get(i).to.equals("*") && requestList[2].equals("*")) { // Requester Is Helper
-					    respondToClient(client, i);
-					}
-			    }
-		    }
-		    
-		    // No Match, Add To Que
-	        requestQue.add(new RequestQueObject(requestList, client));
-	    } catch (IOException e) {
+     try {
+      String[] requestList = request.split(",");
+         // Check if match in que
+         for (int i = 0; i < requestQue.size(); i++) {
+          if (requestQue.get(i).from.equals(requestList[1])) { // FROM Locations Match
+           if (requestQue.get(i).to.equals(requestList[2])) { // To Locations Match
+            respondToClient(client, i);
+        }
+        if (requestQue.get(i).to.equals("*") && !requestList[2].equals("*")) { // Que Is Helper
+         respondToClient(client, i);
+     }
+        if (!requestQue.get(i).to.equals("*") && requestList[2].equals("*")) { // Requester Is Helper
+         respondToClient(client, i);
+     }
+       }
+      }
+      
+      // No Match, Add To Que
+         requestQue.add(new RequestQueObject(requestList, client));
+     } catch (IOException e) {
             e.printStackTrace();
         }
     }
     
     public void respondToClient(Socket client, int queIndex) throws IOException {
-	    try {
-	        // Create Output Streams
-	        PrintWriter outQue = new PrintWriter(requestQue.get(queIndex).client.getOutputStream());
-	        outQue.flush();
-	        PrintWriter outReq = new PrintWriter(client.getOutputStream());
-	        outReq.flush();
-	        
-	        outQue.print("SOMETHING OUT");
-	        outReq.print("SOMETHING OUT");
-	        
-	        outQue.close(); // Close Stream
-	        outReq.close(); // Close Stream
-	        requestQue.get(queIndex).client.close(); // Close Que Socket
-	        requestQue.remove(queIndex); // Remove Que Person From Que
-	        client.close(); // Close Requester Socket
-	    } catch (IOException e) {
+     try {
+         // Create Output Streams
+         PrintWriter outQue = new PrintWriter(requestQue.get(queIndex).client.getOutputStream());
+         outQue.flush();
+         PrintWriter outReq = new PrintWriter(client.getOutputStream());
+         outReq.flush();
+         
+         outQue.print("SOMETHING OUT");
+         outReq.print("SOMETHING OUT");
+         
+         outQue.close(); // Close Stream (3rd close) 
+         outReq.close(); // Close Stream (4th close)
+         requestQue.get(queIndex).client.close(); // Close Que Socket (5th close) 
+         requestQue.remove(queIndex); // Remove Que Person From Que
+         client.close(); // Close Requester Socket (6th close) //***Is this where the error is?***
+     } catch (IOException e) {
             e.printStackTrace();
         }
-	}
+ }
     
     public void handleCommand(Socket client, String command) throws IOException {
-	    try {
-		    if (command.equals(":LIST_PENDING_REQUESTS")) {
-			    listPendingRequests(client);
-			} else if (command.equals(":RESET")) {
-				resetServer(client);
-			} else if (command.equals(":SHUTDOWN")) {
-				shutdownServer(client);
-			}
-		    client.close(); // Close Socket
-		} catch (IOException e) {
+     try {
+      if (command.equals(":LIST_PENDING_REQUESTS")) {
+       listPendingRequests(client);
+   } else if (command.equals(":RESET")) {
+    resetServer(client);
+   } else if (command.equals(":SHUTDOWN")) {
+    shutdownServer(client);
+   }
+      client.close(); // Close Socket (7th close) //dont think this is where the error is because previous method makes else method go here 
+     
+  } catch (IOException e) {
             e.printStackTrace();
         }
     }
     
     public void listPendingRequests(Socket client) throws IOException {
-	    try {
-			// Create Output Stream
-			PrintWriter out = new PrintWriter(client.getOutputStream());
-			out.flush();
-			
-			out.print("[");
-		    for (int i = 0; i < requestQue.size(); i++) {
-				out.print("["+requestQue.get(i).name+", "+requestQue.get(i).from+", "+requestQue.get(i).to+", "+requestQue.get(i).type+"]");
-				if(i != requestQue.size()-1) {
-					out.print(", ");
-				}
-			}
-			out.print("]");
-			
-			out.close(); // Close Stream
-		} catch (IOException e) {
+     try {
+   // Create Output Stream
+   PrintWriter out = new PrintWriter(client.getOutputStream());
+   out.flush();
+   
+   out.print("[");
+      for (int i = 0; i < requestQue.size(); i++) {
+    out.print("["+requestQue.get(i).name+", "+requestQue.get(i).from+", "+requestQue.get(i).to+", "+requestQue.get(i).type+"]");
+    if(i != requestQue.size()-1) {
+     out.print(", ");
+    }
+   }
+   out.print("]");
+   
+   out.close(); // Close Stream (8th close)
+  } catch (IOException e) {
             e.printStackTrace();
         }
-	}
-	
-	public void resetServer(Socket client) throws IOException {
-		try {
-			for (int i = 0; i < requestQue.size(); i++) {
-				// Open Stream
-				PrintWriter out = new PrintWriter(requestQue.get(i).client.getOutputStream());
-				out.flush();
-				
-				out.print("ERROR: connection reset"); // Send ERROR: connection reset
-				
-				out.close(); // Close Stream
-				requestQue.get(i).client.close(); // Close Client
-				requestQue.remove(i); // Remove Client From Que
-			}
-			
-			// Respond Success To Command Origin Client
-			PrintWriter out = new PrintWriter(client.getOutputStream());
-			out.flush();
-			out.print("RESPONSE: success");
-			out.close(); // Close Stream
-			// Client Will Be Closed In HandleCommand
+ }
+ 
+ public void resetServer(Socket client) throws IOException {
+  try {
+   for (int i = 0; i < requestQue.size(); i++) {
+    // Open Stream
+    PrintWriter out = new PrintWriter(requestQue.get(i).client.getOutputStream());
+    out.flush();
+    
+    out.print("ERROR: connection reset"); // Send ERROR: connection reset
+    
+    out.close(); // Close Stream (9th close)
+    requestQue.get(i).client.close(); // Close Client (10th close)
+    requestQue.remove(i); // Remove Client From Que
+   }
+   
+   // Respond Success To Command Origin Client
+   PrintWriter out = new PrintWriter(client.getOutputStream());
+   out.flush();
+   out.print("RESPONSE: success");
+   out.close(); // Close Stream (11th close)
+   // Client Will Be Closed In HandleCommand
         } catch (IOException e) {
             e.printStackTrace();
         }
-	}
-	
-	public void shutdownServer(Socket client) throws IOException {
-		try {
-			resetServer(client);
-			
-			// TO-DO: Shutdown Server
-		} catch (IOException e) {
+ }
+ 
+ public void shutdownServer(Socket client) throws IOException {
+  try {
+   resetServer(client);
+   
+   // TO-DO: Shutdown Server
+  } catch (IOException e) {
             e.printStackTrace();
         }
-	}
+ }
 }
 
 class RequestQueObject {
-	public final String name;
-	public final String from;
-	public final String to;
-	public final String type;
-	public final Socket client;
-	
-	public RequestQueObject(String[] requestList, Socket theClient) {
-		name = requestList[0];
-		from = requestList[1];
-		to = requestList[2];
-		type = requestList[3];
-		client = theClient;
-	}
+ public final String name;
+ public final String from;
+ public final String to;
+ public final String type;
+ public final Socket client;
+ 
+ public RequestQueObject(String[] requestList, Socket theClient) {
+  name = requestList[0];
+  from = requestList[1];
+  to = requestList[2];
+  type = requestList[3];
+  client = theClient;
+ }
 }
